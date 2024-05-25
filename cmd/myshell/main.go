b8a6e47333a7a707cab43fcbdf02d4335251b80f
feliposz <feliposz@gmail.com>
2024-05-24 22:13:44 -0300

    The echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -23,13 +23,17 @@ func main() {
 		}
 		input = strings.TrimRight(input, "\n")
 		cmd := strings.Fields(input)
-		if cmd[0] == "exit" {
+		switch cmd[0] {
+		case "exit":
 			var exitCode int
 			if len(cmd) > 1 {
 				exitCode, _ = strconv.Atoi(cmd[1])
 			}
 			os.Exit(exitCode)
+		case "echo":
+			fmt.Printf("%s\n", strings.Join(cmd[1:], " "))
+		default:
+			fmt.Printf("%s: command not found\n", cmd[0])
 		}
-		fmt.Printf("%s: command not found\n", input)
 	}
 }
